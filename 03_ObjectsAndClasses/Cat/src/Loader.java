75e74c14a9cf943fdd9fb3524773f903924cb767
Alexey <alexpestov89@yandex.ru>
2020-04-17 23:31:16 +0300

    lesson 1

--- 03_ObjectsAndClasses/Cat/src/Loader.java
+++ 03_ObjectsAndClasses/Cat/src/Loader.java
@@ -4,7 +4,26 @@ public class Loader
     public static void main(String[] args)
     {
         Cat cat = new Cat();
+        Cat cat2 = new Cat();
+        Cat cat3 = new Cat();
+        Cat cat4 = new Cat();
+        Cat cat5 = new Cat();
+
+        System.out.println(cat.getWeight());
+        System.out.println(cat2.getWeight());
+        System.out.println(cat3.getWeight());
+        System.out.println(cat4.getWeight());
+        System.out.println(cat5.getWeight());
+
+        while (cat.getStatus() != "Exploded")   {
+            cat.feed(100.0);
+        }
+
+        while (cat2.getStatus() != "Dead")   {
+            cat2.meow();
+        }
 
         System.out.println(cat.getStatus());
+        System.out.println(cat2.getStatus());
     }
 }
