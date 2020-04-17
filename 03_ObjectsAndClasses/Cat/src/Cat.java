e80fc70a93b0f7c30414a3e43ee2590e60d8a481
Alexey <alexpestov89@yandex.ru>
2020-04-17 23:37:10 +0300

    lesson 2

--- 03_ObjectsAndClasses/Cat/src/Cat.java
+++ 03_ObjectsAndClasses/Cat/src/Cat.java
@@ -1,14 +1,14 @@
 
-public class Cat
-{
+public class Cat {
     private double originWeight;
     private double weight;
 
     private double minWeight;
     private double maxWeight;
 
-    public Cat()
-    {
+    private double allAmountEat = 0;
+
+    public Cat() {
         weight = 1500.0 + 3000.0 * Math.random();
         originWeight = weight;
         minWeight = 1000.0;
@@ -16,39 +16,41 @@ public class Cat
 
     }
 
-    public void meow()
-    {
+    public void meow() {
         weight = weight - 1;
         System.out.println("Meow");
     }
 
-    public void feed(Double amount)
-    {
+    public void feed(Double amount) {
+        weight = weight + amount;
+        allAmountEat += amount;
+    }
+
+    public double getAllAmountEat() {
+        return allAmountEat;
+    }
+
+    public void drink(Double amount) {
         weight = weight + amount;
     }
 
-    public void drink(Double amount)
-    {
-        weight = weight + amount;
+    public void pee ()  {
+        weight -= 1;
+        System.out.println("Сходила в туалет");
     }
 
-    public Double getWeight()
-    {
+    public Double getWeight() {
         return weight;
     }
 
-    public String getStatus()
-    {
-        if(weight < minWeight) {
+    public String getStatus() {
+        if (weight < minWeight) {
             return "Dead";
-        }
-        else if(weight > maxWeight) {
+        } else if (weight > maxWeight) {
             return "Exploded";
-        }
-        else if(weight > originWeight) {
+        } else if (weight > originWeight) {
             return "Sleeping";
-        }
-        else {
+        } else {
             return "Playing";
         }
     }
--- 03_ObjectsAndClasses/Cat/src/Loader.java
+++ 03_ObjectsAndClasses/Cat/src/Loader.java
@@ -4,26 +4,10 @@ public class Loader
     public static void main(String[] args)
     {
         Cat cat = new Cat();
-        Cat cat2 = new Cat();
-        Cat cat3 = new Cat();
-        Cat cat4 = new Cat();
-        Cat cat5 = new Cat();
-
-        System.out.println(cat.getWeight());
-        System.out.println(cat2.getWeight());
-        System.out.println(cat3.getWeight());
-        System.out.println(cat4.getWeight());
-        System.out.println(cat5.getWeight());
-
-        while (cat.getStatus() != "Exploded")   {
-            cat.feed(100.0);
+        cat.feed(150.0);
+        for (int i = 0; i <= 10; i++)   {
+            cat.pee();
         }
-
-        while (cat2.getStatus() != "Dead")   {
-            cat2.meow();
-        }
-
-        System.out.println(cat.getStatus());
-        System.out.println(cat2.getStatus());
+        System.out.println(cat.getAllAmountEat());
     }
 }
