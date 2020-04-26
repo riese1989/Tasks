ede453c8d7357f79dd26caed4ac84e40c73cb12c
Alexey <alexpestov89@yandex.ru>
2020-04-26 23:18:33 +0300

    Task 4.21 2

--- 04_NumbersStringsAndDates/StringExperiments/src/Loader.java
+++ 04_NumbersStringsAndDates/StringExperiments/src/Loader.java
@@ -4,7 +4,12 @@ public class Loader
     public static void main(String[] args)
     {
         String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
-
-        System.out.println(text);
+        int indexRublesEndVasya = text.indexOf(" рублей");
+        int indexRublesStartVasya = text.substring(0,indexRublesEndVasya).lastIndexOf(" ");
+        String casheVasya = text.substring(indexRublesStartVasya, indexRublesEndVasya).trim();
+        int indexRublesEndMasha = text.lastIndexOf(" рублей");
+        int indexRublesStartMasha = text.substring(0, indexRublesEndMasha).lastIndexOf(" ");
+        String casheMasha = text.substring(indexRublesStartMasha, indexRublesEndMasha).trim();
+        System.out.println(Integer.parseInt(casheVasya) + Integer.parseInt(casheMasha));
     }
 }
