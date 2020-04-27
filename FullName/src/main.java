45bda571a2a21532a1f01a4eb115b1ba4af6650a
Alexey <alexpestov89@yandex.ru>
2020-04-28 00:21:09 +0300

    Task 4.24 3

--- FullName/src/main.java
+++ FullName/src/main.java
@@ -5,15 +5,15 @@ public class main {
         System.out.println("Введите ФИО");
         Scanner scanner = new Scanner(System.in);
         String str = scanner.nextLine();
-        int indexStart = str.indexOf(" ");
-        int indexEnd = str.lastIndexOf(" ");
-        String family = str.substring(0,indexStart).trim().toLowerCase();
+        String regex = "[^A-Za-zА-Яа-я]";
+        String[] splitStr = str.replaceAll(regex," ").split("\\s+");
+        String family = splitStr[0].trim().toLowerCase();
         String correctFamily = family.replaceFirst(family.substring(0,1).trim(), family.substring(0,1).trim().toUpperCase());
         System.out.println("Фамилия: " + correctFamily);
-        String name = str.substring(indexStart, indexEnd).trim().toLowerCase();
+        String name = splitStr[1].trim().toLowerCase();
         String correctName = name.replaceFirst(name.substring(0,1).trim(),name.substring(0,1).trim().toUpperCase());
         System.out.println("Имя: " + correctName);
-        String surname = str.substring(indexEnd).trim();
+        String surname = splitStr[2].trim();
         String correctSurname = surname.replaceFirst(surname.substring(0,1).trim(), surname.substring(0,1).trim().toUpperCase());
         System.out.println("Отчество: " + correctSurname);
     }
