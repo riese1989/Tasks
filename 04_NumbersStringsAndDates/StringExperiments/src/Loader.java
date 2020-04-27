05c494b4b244e3215eb44a6351da0112ac6ae531
Alexey <alexpestov89@yandex.ru>
2020-04-27 23:44:04 +0300

    Task 4.25 1

--- 04_NumbersStringsAndDates/StringExperiments/src/Loader.java
+++ 04_NumbersStringsAndDates/StringExperiments/src/Loader.java
@@ -1,15 +1,15 @@
+import java.util.jar.JarOutputStream;
 
 public class Loader
 {
     public static void main(String[] args)
     {
+        int sum = 0;
         String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
-        int indexRublesEndVasya = text.indexOf(" рублей");
-        int indexRublesStartVasya = text.substring(0,indexRublesEndVasya).lastIndexOf(" ");
-        String casheVasya = text.substring(indexRublesStartVasya, indexRublesEndVasya).trim();
-        int indexRublesEndMasha = text.lastIndexOf(" рублей");
-        int indexRublesStartMasha = text.substring(0, indexRublesEndMasha).lastIndexOf(" ");
-        String casheMasha = text.substring(indexRublesStartMasha, indexRublesEndMasha).trim();
-        System.out.println(Integer.parseInt(casheVasya) + Integer.parseInt(casheMasha));
+        String[] casheText = text.replaceAll("[^0-9]"," ").trim().split("\\s+");
+        for (int i = 0; i < casheText.length; i++)  {
+            sum += Integer.parseInt(casheText[i]);
+        }
+        System.out.println(sum);
     }
 }
