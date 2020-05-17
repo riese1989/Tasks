2fb44b1cd72740643b6b5849448c8b924c797596
Alexey <alexpestov89@yandex.ru>
2020-05-17 12:05:08 +0300

    Task 7.8.2 v2

--- 07_AdvancedOOPFeatures/Airports/src/Main.java
+++ 07_AdvancedOOPFeatures/Airports/src/Main.java
@@ -1,21 +1,20 @@
-import com.skillbox.airport.Aircraft;
 import com.skillbox.airport.Airport;
-import com.skillbox.airport.Flight;
+import com.skillbox.airport.Terminal;
 
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-        Integer code = 1000;
-        SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm");
+        Airport airport = Airport.getInstance();
         Date curDate = new Date();
-        ArrayList <Flight> fly = new ArrayList<>();
-        for (int numberAircraft = 0; numberAircraft <=50; numberAircraft++)    {
-            Aircraft aircraft = new Aircraft(Airport.getInstance().getAllAircrafts().get(0).getModel());
-            fly.add(new Flight(code.toString(), Flight.Type.DEPARTURE,new Date (curDate.getTime() + numberAircraft * 1000 * 420) ,aircraft));
-        }
-        fly.stream().filter(f -> f.getDate().getTime() - curDate.getTime() <= 2 * 1000 * 3600).forEach(f -> System.out.println(f.getAircraft() + " "+dateFormat.format(f.getDate())));
-    }
+        String string = "\n==================";
+        SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm");
+        for (Terminal terminal : airport.getTerminals())    {
+            System.out.println("Вылеты из " + terminal.getName() + string);
+            terminal.getFlights().stream().filter(f -> f.getDate().getTime() - curDate.getTime() <= 2 * 1000 * 3600).forEach(f -> System.out.println(f.getAircraft() + " "+dateFormat.format(f.getDate())));
+            System.out.println(string);
+        }}
+
+
 }
