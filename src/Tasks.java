df951223056c436eadd39bf89cd9ec25a23dd1d3
Alexey.Pestov <alexey.pestov@x5.ru>
2020-06-01 18:13:24 +0300

    Add Tasks status

--- src/Tasks.java
+++ src/Tasks.java
@@ -1,6 +1,25 @@
 public class Tasks {
     private static Integer countTasksAll;
     private static Integer noneAppTasks;
+    private TaskStatus status;
+    private Employee employee;
+    private String number;
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
 
     public static Integer getNoneAppTasks() {
         return noneAppTasks;
@@ -16,5 +35,21 @@ public class Tasks {
 
     public static void setCountTasksAll(Integer countTasksAll) {
         Tasks.countTasksAll = countTasksAll;
+    }
+
+    public TaskStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(TaskStatus status) {
+        this.status = status;
+    }
+    public void switchStatus()  {
+        if (this.getStatus() == TaskStatus.DONE)    {
+            this.setStatus(TaskStatus.NOTE_DONE);
+        }
+        else    {
+            this.setStatus(TaskStatus.DONE);
+        }
     }
 }
